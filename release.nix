40787493b3f24411cd6cad415d2d054ecb2ca147
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-12-28 05:57:51 -0800

    nix-build release.nix works

--- release.nix
+++ release.nix
@@ -1,8 +1,10 @@
 let
   sources    = import ./nix/sources.nix {};
   pkgs       = import sources.nixpkgs   {};
-  diamond    = pkgs.callPackage sources.diamond  {};
+  diamond    = pkgs.callPackage sources.diamond {};
+
   mcl        = pkgs.callPackage ../mcl-nix        {}; # TODO upload repo and import via niv
+  fastme     = pkgs.callPackage ../fastme-nix     {}; # TODO upload repo and import via niv
   ncbi-blast = pkgs.callPackage ../ncbi-blast-nix {}; # TODO upload repo and import via niv
 
 in pkgs.callPackage ./default.nix {
