b7824973546509b90f45173828b3ecbd1a134be8
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-12-28 05:37:06 -0800

    wip: copy nix files from ortholang

---
+++ release.nix
@@ -0,0 +1,11 @@
+let
+  sources    = import ./nix/sources.nix {};
+  pkgs       = import sources.nixpkgs   {};
+  diamond    = pkgs.callPackage sources.diamond  {};
+  mcl        = pkgs.callPackage ../mcl-nix        {}; # TODO upload repo and import via niv
+  ncbi-blast = pkgs.callPackage ../ncbi-blast-nix {}; # TODO upload repo and import via niv
+
+in pkgs.callPackage ./default.nix {
+  inherit (pkgs.lib) makeBinPath;
+  inherit mcl fastme ncbi-blast diamond;
+}
